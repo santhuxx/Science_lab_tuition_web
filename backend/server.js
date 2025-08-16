6e74456090df0f6be684d2212ffa3f900dd57dce
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-08-16 15:56:50 +0530

    Update CORS origins to include Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS Options
 const corsOptions = {
-  origin: "http://localhost:5173", // frontend URL
+  origin: ["http://localhost:5173","https://science-lab-tuition-web-s4yc.vercel.app"], // frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // allow cookies if needed
